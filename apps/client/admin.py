12e11fed4ee04b2717ee262a9623c5016b65739c
BelenBRC <belenrob.alumn@cescristorey.com>
2024-06-06 21:28:52 +0200

    Styled reports in admin views

--- apps/client/admin.py
+++ apps/client/admin.py
@@ -1,10 +1,28 @@
+from django import forms
 from django.contrib import admin
 
 from apps.client.models import Client
+from apps.reports.models import Report
 
 # Register your models here.
 
+class ReportInline(admin.TabularInline):
+    model = Report
+    extra = 0
+    fields = ['date', 'weight', 'training_compliance', 'training_satisfaction', 'diet_compliance', 'diet_satisfaction', 'diet_changes', 'training_observations', 'other_observations', 'front_picture', 'right_side_picture', 'left_side_picture']
+    readonly_fields = ['date']
+    ordering = ['-date']
+    
+    # Smaller text area
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        formfield = super().formfield_for_dbfield(db_field, **kwargs)
+        if db_field.name == 'diet_changes' or db_field.name == 'training_observations' or db_field.name == 'other_observations':
+            formfield.widget = forms.Textarea(attrs={'rows': 3, 'cols': 15})
+        
+        return formfield
+
 class ClientAdmin(admin.ModelAdmin):
+    inlines = [ReportInline]
     list_display = ['full_name_display', 'user', 'coach', 'birthdate', 'city', 'occupation', 'age_display']
     list_filter = ['coach', 'city', 'occupation']
     search_fields = ['user__username', 'city', 'occupation']
@@ -17,5 +35,7 @@ class ClientAdmin(admin.ModelAdmin):
     def full_name_display(self, obj):
         return obj.full_name
     full_name_display.short_description = 'Nombre completo'
+    
+    
     
 admin.site.register(Client, ClientAdmin)
--- apps/reports/admin.py
+++ apps/reports/admin.py
@@ -3,4 +3,10 @@ from django.contrib import admin
 from apps.reports.models import Report
 
 # Register your models here.
-admin.site.register(Report)
+class ReportAdmin(admin.ModelAdmin):
+    list_display = ['client', 'date', 'weight']
+    list_filter = ['client', 'date']
+    search_fields = ['client__full_name', 'date']
+    ordering = ['client', 'date']
+
+admin.site.register(Report, ReportAdmin)
