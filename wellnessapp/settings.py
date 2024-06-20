b5540c7e189498ba8d673d30996c5bb10fe1950c
BelenBRC <belenrob.alumn@cescristorey.com>
2024-06-20 17:51:23 +0200

    Allow railway host

--- wellnessapp/settings.py
+++ wellnessapp/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', default='django-insecure-#&')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG', default=True)
+DEBUG = os.environ.get('DJANGO_DEBUG', default=False)
 
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', ['localhost', '127.0.0.1'])
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', ['localhost', '127.0.0.1', 'mytraincoach.up.railway.app'])
 
 
 # Application definition
